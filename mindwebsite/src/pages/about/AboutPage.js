1d2d87f8202ad0b78353386ba53d24e9abc6f42d
shaqattack13 <57778130+shaqattack13@users.noreply.github.com>
2021-07-10 04:20:36 -0500

    Began work on About page

--- mindwebsite/src/pages/about/AboutPage.js
+++ mindwebsite/src/pages/about/AboutPage.js
@@ -1,10 +1,31 @@
 import './AboutPage.css';
 import '../../App.css';
+import textList from './text';
 
 export default function AboutPage() {
   return (
     <div className='aboutPage'>
-      <h1>TEST: About Us</h1>
+
+      <div className='aboutInfo'>
+        <div id='aboutHeader'>
+          <h1>About Us</h1>
+        </div>
+
+        <div>
+          <p>{textList[0]}</p>
+        </div>
+
+        <ol>
+          <li>{textList[1]}</li>
+          <li>{textList[2]}</li>
+          <li>{textList[3]}</li>
+        </ol>
+      </div>
+
+      <div className='officersInfo'>
+        <h2>MIND Officers</h2>
+      </div>
+
     </div>
   );
 };
---
+++ mindwebsite/src/pages/about/text.js
@@ -0,0 +1,16 @@
+const aboutText = "Purdue MIND’s purpose is to promote and grow the biomedical engineering community at Purdue by providing \
+undergraduate students the opportunity to engage in hands-on activities, discuss current research, and network with a \
+wide range of faculty both within and outside of Purdue including professors, physicians, and engineers. We are a unique, \
+multidisciplinary student organization that seeks to innovate and develop medical technology. Our overall goals are listed below:";
+
+const goal1 = "To give Purdue undergraduates extensive experience with medical technology market research, design, prototyping, \
+validation, regulatory approval, intellectual property management, and transition to market.";
+
+const goal2 = "To allow highly motivated students to network with each other and with professors and medical professionals and \
+to collaborate on a significant, long-term, medically-focused project."
+
+const goal3 = "To recognize the work done on the project through submission to national design competitions."
+
+let textList = [ aboutText, goal1, goal2, goal3 ]
+
+export default textList;
