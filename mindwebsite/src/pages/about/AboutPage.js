e502651fed85c82c494bf1cbf535a796aa7abb14
shaqattack13 <57778130+shaqattack13@users.noreply.github.com>
2021-07-11 21:08:14 -0500

    About page update 2: up to officer profile stuff/current working on it

--- mindwebsite/src/pages/about/AboutPage.js
+++ mindwebsite/src/pages/about/AboutPage.js
@@ -12,19 +12,36 @@ export default function AboutPage() {
         </div>
 
         <div>
-          <p>{textList[0]}</p>
+          <p>{textList.aboutTexts[0]}</p>
         </div>
 
         <ol>
-          <li>{textList[1]}</li>
-          <li>{textList[2]}</li>
-          <li>{textList[3]}</li>
+          <li>{textList.aboutTexts[1]}</li>
+          <li>{textList.aboutTexts[2]}</li>
+          <li>{textList.aboutTexts[3]}</li>
         </ol>
       </div>
 
       <div className='officersInfo'>
         <h2>MIND Officers</h2>
+
+        <OfficerProfile name={textList.officerNames[0]} pos={textList.officerPoses[0]} desc={textList.officerDescs[0]} />
+        <OfficerProfile name={textList.officerNames[1]} pos={textList.officerPoses[1]} desc={textList.officerDescs[1]} />
+        <OfficerProfile name={textList.officerNames[2]} pos={textList.officerPoses[2]} desc={textList.officerDescs[2]} />
+
       </div>
+
+    </div>
+  );
+};
+
+function OfficerProfile({name, pos, desc}) {
+  return (
+    <div className='officerProfile'>
+
+      <h3>{name}</h3>
+      <h4>{pos}</h4>
+      <p>{desc}</p>
 
     </div>
   );
