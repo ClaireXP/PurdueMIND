ee4f2117d3b38826372cd83d92af674bfed11429
Claire Poukey <cpoukey@google.com>
2021-10-17 17:22:44 -0400

    Quick fixes

--- mindwebsite/src/pages/alumni/components/Alumni.js
+++ mindwebsite/src/pages/alumni/components/Alumni.js
@@ -12,23 +12,16 @@ export default function AlumniProfile(
     name,
     position,
     major,
-    description,
-    outlook,
-    linkedIn
   }) {
   return (
     <div className='profile'>
       <img className='profilePic' id={id} src={imgSrc} alt={alt} />
-      <a href={linkedIn}><button id="linkedIn">LinkedIn</button></a>
 
       <div id='eboardInfo'>
         <h3 className='name'>{name}</h3>
         <h4 className='position'>{position}</h4>
         <h5 className='major'>{major}</h5>
-        {/* <h5 className='description'>{description}</h5> */}
         <br/>
-        {/* <a className='outlook' href={`mailto:${outlook}`}>{outlook}</a> */}
-        <div id='spacer'/>
       </div>
     </div>
   );
